d1d429d5ad025ed5b186c3c211514066014025da
ariel taylor <ariel.taylorr@gmail.com>
2020-11-16 13:01:50 -0800

    Build new_move_positions

--- knights_travail/knights_travail.rb
+++ knights_travail/knights_travail.rb
@@ -9,6 +9,7 @@ class KnightPathFinder
         @board = Array.new(8){Array.new(8)} # poly tree
         @root_node = PolyTreeNode.new(pos)
         self.build_move_tree
+        @considered_positions = [pos]
     end
 
     def [](pos)
@@ -21,24 +22,56 @@ class KnightPathFinder
         @board[x][y] = val
     end
 
+    self.valid_moves(pos)
+        row, col = pos
+
+        valids_filtered = []
+    
+        valids = [[row + 2, col - 1], [row + 2, col + 1], [row - 1, col + 2], [row - 1, col -2]        
+        [row - 2, col + 1], [row - 2, col - 1], [row + 1, col - 2],[row + 1, col + 2]]
+
+        valids.each do |position|
+            next if position[0] < 0 || position[1] < 0 
+            next if position[0] > 7 || position[1] > 7
+            valids_filtered << position 
+        end
+        valids_filtered
+        #limited rows:
+        # 0, 1, 6, 7
+        #limited cols
+        # 0, 1, 6, 7
+
+    end
+
+    def new_move_positions(pos)
+        valids = self.class.valid_moves(pos)
+        new_positions = []
+
+        valids.each do |position|
+            if !@considered_positions.include?(position)
+                @considered_positions << position
+                new_positions << position
+            end
+        end
+        new_positions
+
+    end
+
     def build_move_tree
+        @root_node.add_child(new_move_positions(@pos))
     end
 
     def find_path
 
     end
 
-    self.valid_moves(pos)
-        row, col = pos
+
+         
     
-       valids = [[row + 2, col - 1], [row + 2, col + 1], [row - 1, col + 2], [row - 1, col -2]        
-        [row - 2, col + 1], [row - 2, col - 1], [row + 1, col - 2],[row + 1, col + 2]]
 
-    end
+end
 
-
-
-         # # valid_moves = []
+# # valid_moves = []
         # # (1..2).each do |row|
         # #     2.down_to(1).each do |col|
         # #         valid_moves << [row,col]
@@ -55,8 +88,3 @@ class KnightPathFinder
         # else
         #     return false
         # end   
-    
-
-end
-
-
