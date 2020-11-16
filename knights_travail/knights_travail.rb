152ad8881fe223a85f1b989243faa2e28a7e5dad
gabriel <gabeh.reiter@gmail.com>
2020-11-16 11:30:40 -0500

    added initialize and vars

--- knights_travail/knights_travail.rb
+++ knights_travail/knights_travail.rb
@@ -1,7 +1,25 @@
-class KnightPathFidner
+require_relative ../skeleton/lib PolyTreeNode
 
-    def initialize
-        @board = Array.new(8){Array.new(PolyTreeNode.new())} # poly tree
+class KnightPathFinder
+
+    def initialize(pos)
+        @board = Array.new(8){Array.new(8,PolyTreeNode.new())} # poly tree
+        self.root_node = @board(pos)
+
+        @kpf = KnightPathFinder.new(pos)
+        build_move_tree(self.root_node)
+    end
+
+    def [](pos)
+        x,y = pos
+        @board[x][y]
+    end
+
+    def []=(pos,val)
+        x,y = pos
+        @board[x][y] = val
     end
 
 end
+
+
